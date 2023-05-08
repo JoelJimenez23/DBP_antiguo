ad8bbad70273a152dd26bf3d09c093e336aee9c9
JoelJimenez23 <119914405+JoelJimenez23@users.noreply.github.com>
2023-05-08 15:59:37 -0500

    b

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from flask import Flask,render_template,jsonify,request
-from flask_sqlalchemy import SQLAlchemy, CheckConstraint
+from flask_sqlalchemy import SQLAlchemy
 import uuid
-import os
 from datetime import  datetime
 import sys
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:230204@localhost:5432/dbp'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:230204@localhost:5432/dpb'
 app.config['UPLOAD_FOLDER'] = 'static/usuarios'
 db = SQLAlchemy(app)
 ALLOWED_EXTENSIONS = {'png','jpeg','jpg','gif'}
@@ -20,7 +19,7 @@ class User(db.Model):
     id = db.Column(db.String(36),primary_key=True, default=lambda: str(uuid.uuid4()), server_default=db.text("uuid_generate_v4()"))
     nickname = db.Column(db.String(100),unique=False,nullable=False)
     e_mail = db.Column(db.String(100),primary_key=True,nullable=False,unique=True)
-    saldo = db.Column(db.Integer,CheckConstraint('saldo >= 0',name='saldo_check'),nullable=False)
+    saldo = db.Column(db.Integer,nullable=False)
     created_at = db.Column(db.DateTime(timezone=True), nullable=False, server_default=db.text("now()"))
 
     def __init__(self,nickname,e_mail,saldo,created_at):
