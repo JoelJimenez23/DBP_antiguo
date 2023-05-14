5307570e271ad99fddf550381c861b87dd3dd590
Josroyal <josue.arbulu@utec.edu.pe>
2023-05-13 22:27:20 -0500

    GAAAAAAAAAAAAAAAAA

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 import uuid
 from datetime import datetime
 import sys
+import getpass
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:josue2003@localhost:5432/skinloot'
@@ -23,13 +24,15 @@ class User(db.Model):
     id = db.Column(db.String(36),primary_key=True, default=lambda: str(uuid.uuid4()), server_default=db.text("uuid_generate_v4()"))
     nickname = db.Column(db.String(100),unique=False,nullable=False)
     e_mail = db.Column(db.String(100),primary_key=True,nullable=False,unique=True)
+    password = db.Column(db.String(100),nullable=False)
     saldo = db.Column(db.Integer,nullable=False)
     created_at = db.Column(db.DateTime(timezone=True), nullable=False, server_default=db.text("now()"))
 
-    def __init__(self,nickname,e_mail,saldo):
+    def __init__(self,nickname,e_mail,saldo,password):
         self.nickname = nickname
         self.e_mail = e_mail    
         self.saldo = saldo
+        self.password = password
         self.created_at = datetime.utcnow()
     
     def serialize(self):
@@ -38,6 +41,7 @@ class User(db.Model):
             'nickname' : self.nickname,
             'e_mail' : self.e_mail,
             'saldo' : self.saldo,
+            'password' : self.password,
             'created_at':self.created_at
         }
     
@@ -61,9 +65,10 @@ def register_user():
     try:
         nickname = request.form.get('nickname')
         e_mail = request.form.get('e_mail')
+        password = getpass.getpass('Password: ')
         saldo = request.form.get('saldo')
 
-        user = User(nickname,e_mail,saldo)
+        user = User(nickname,e_mail,password,saldo)
         db.session.add(user)
         db.session.commit()
 
@@ -72,7 +77,7 @@ def register_user():
         print(e)
         print(sys.exc_info())
         db.session.rollback()
-        return jsonify({'succes':False, 'message':'Error creating user'}),500
+        return jsonify({'success':False, 'message':'Error creating user'}),500
     finally:
         db.session.close()
 
