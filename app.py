ef6ac8941d765f9bf43fdfcc460e340a9a671c7a
JoelJimenez23 <119914405+JoelJimenez23@users.noreply.github.com>
2023-05-15 18:54:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 import uuid
 from datetime import datetime
 import sys
+import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:546362@localhost:5432/skinloot'
-app.config['UPLOAD_FOLDER'] = 'static/usuarios'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:230204@localhost:5432/skinloot'
+app.config['UPLOAD_FOLDER'] = 'static/skins'
 db = SQLAlchemy(app)
-ALLOWED_EXTENSIONS = {'png','jpeg','jpg','gif'}
+ALLOWED_EXTENSIONS = {'png','jpeg','jpg'}
 
 
 # codigo correo nickname saldo
@@ -39,7 +40,6 @@ class User(db.Model):
             'saldo' : self.saldo,
             'created_at':self.created_at
         }
-    
 class Skin(db.Model):
     __tablename__='skins'
     hash = db.Column(db.String(36),primary_key=True, default=lambda: str(uuid.uuid4()), server_default=db.text("uuid_generate_v4()"))
@@ -48,6 +48,22 @@ class Skin(db.Model):
     game_name = db.Column(db.String(100),unique=False,nullable=False)
     company_name = db.Column(db.String(100),unique=False,nullable=False)
     image = db.Column(db.String(500),unique=False,nullable=False)
+
+    def __init__(self, name,category,game_name,company_name,image):
+        self.name = name
+        self.category = category
+        self.game_name = game_name
+        self.company_name = company_name    
+    
+    def serialize(self):
+        return{
+            'hash':self.hash,
+            'name':self.name,
+            'category':self.category,
+            'game_name':self.game_name,
+            'company_name':self.company_name,
+            'image':self.image
+        }
 
 with app.app_context():db.create_all()
 
@@ -62,25 +78,7 @@ def home():
 @app.route('/register',methods=["GET"])
 def register():
     return render_template('login.html')
-"""
-@app.route('/register-user',methods=["POST"])
-def register_user():
-    try:
-        nickname = request.form.get('nickname')
-        e_mail = request.form.get('e_mail')
-        saldo = request.form.get('saldo')
-        user = User(nickname,e_mail,saldo)
-        db.session.add(user)
-        db.session.commit()
-        return jsonify({'id':user.id,'success':True,'message':'User created successfully!'}),200
-    except Exception as e:
-        print(e)
-        print(sys.exc_info())
-        db.session.rollback()
-        return jsonify({'succes':False, 'message':'Error creating user'}),500
-    finally:
-        db.session.close()
-"""
+
 @app.route('/register-user', methods=["POST"])
 def register_user():
     try:
@@ -98,6 +96,56 @@ def register_user():
         return jsonify({'success': False, 'message': 'Error creating user'}), 500
     finally:
         db.session.close()
+
+@app.route('/skins',methods=['GET'])
+def skins():
+    try:
+        skins = Skin.query.all()
+        skins_serialized = [skin.serialize() for skin in skins]
+        return jsonify({'success':True,'skins':skins_serialized}),200
+    except:
+        return jsonify({'success':False})
+    
+@app.route('/new-skin', methods = ['GET'])
+def new_skin():
+    return render_template()
+
+@app.route('/register-skin',methods=['POST'])
+def register_skin():
+    try:
+        name = request.form.get('name')
+        category = request.form.get('category')
+        game_name = request.form.get('game_name')
+        company_name = request.form.get('company_name')
+
+        if 'image' not in request.files:
+            return jsonify({'success':False,'message':'html error'}),400
+        file = request.files['image']
+
+        if file.filename == '':
+            return jsonify({'success':False,'message':'no image selected'}),400
+        
+        if not allowed_file(file.filename):
+            return jsonify({'success':False,'message':'Image format not allowed'}),400
+        
+        skin = Skin(name,category,game_name,company_name)
+        db.session.add(skin)
+        db.session.commit()
+
+        cwd = os.getcwd()
+
+        
+    
+    except:
+
+    finally:
+
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 # Run the app
 if __name__ == '__main__':
